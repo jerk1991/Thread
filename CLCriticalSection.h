fcd54c6274d0e66ed339b9b6d91bcd889975fd78
jerk1991 <18fabragas@gmail.com>
2015-08-15 04:04:20 +0800

    Create CLCriticalSection.h

---
+++ CLCriticalSection.h
@@ -0,0 +1,27 @@
+#incldue "CLMutex.h"
+
+#ifndef _CLCRITICALSECTION_H_
+#define _CLCRITICALSECTION_H_
+
+class CLCriticalSection {
+public:
+	CLCriticalSection(CLMutex* p_Mutex) {
+		if(p_Mutex == 0)
+			throw "CLCriticalSection::CLCriticalSection() error";
+		m_pMutex = p_Mutex;
+		CLStatus s = m_pMutex->lock();
+		if(!s.IsSuccess()) {
+			throw "CLCriticalSection::CLCriticalSection() m_pMutex->lock() error";
+		}
+		
+	}
+	virtual ~CLCriticalSection() {
+		CLStatus s = m_pMutex->Unlock();
+		if(!s.IsSuccess()) {
+			CLLogger::WriteLogMsg("~CLCriticalSection() m_pMutex->Unlock() error",0);
+	}
+private:
+	CLMutex *m_pMutex;
+}
+
+#endif
